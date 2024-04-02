096b611a7db696592270dab8b8fbef9a3fdc2ecd
JCWoodworker <jc@rilocalwoodworks.com>
2024-04-02 17:41:11 -0400

    put auth back in place for GET request to main backend url

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,13 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Auth } from './iam/decorators/auth.decorator';
-import { AuthType } from './iam/enums/auth-type.enum';
 
-@Auth(AuthType.None)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   @Render('index')
-  getIntroMessage(): string {
-    return this.appService.getIntroMessage();
+  root() {
+    return { message: '' };
   }
 }
