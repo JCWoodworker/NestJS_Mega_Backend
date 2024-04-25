31ab4130e53441181d65cb40315cf4408bae57fa
JCWoodworker <jc@rilocalwoodworks.com>
2024-04-24 22:32:11 -0400

    added all resources for coasters and updated module to include new controller, service, entity, and dtos

---
+++ src/subapps/mycuttingboard/resources/services/coasters.service.ts
@@ -0,0 +1,28 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { MycuttingboardCoasters } from '../../entities/mycuttingboardCoasters.entity';
+@Injectable()
+export class CoastersService {
+  constructor(
+    @InjectRepository(MycuttingboardCoasters)
+    private coastersRepository: Repository<MycuttingboardCoasters>,
+  ) {}
+
+  async getCoasterDataById(id: number) {
+    const coaster = await this.coastersRepository.findOne({ where: { id } });
+    if (!coaster) {
+      throw new HttpException('Coaster not found', HttpStatus.NOT_FOUND);
+    }
+    return coaster;
+  }
+
+  async addCoasterData(coasterData: MycuttingboardCoasters) {
+    return await this.coastersRepository.save(coasterData);
+  }
+
+  async deleteCoasterData(id: number) {
+    return await this.coastersRepository.delete({ id });
+  }
+}
