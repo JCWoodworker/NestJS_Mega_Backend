63750b84cd7dc1b247152e2d2a0492386f9e44d8
JCWoodworker <jc@rilocalwoodworks.com>
2024-05-22 18:03:02 -0400

    updated firebase module to use process.env

---
+++ src/iam/authentication/Firebase/firebase.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as admin from 'firebase-admin';
+
+const firebaseProvider = {
+  provide: 'FIREBASE_APP',
+  useFactory: () => {
+    const firebaseConfig = {
+      type: process.env.TYPE,
+      project_id: process.env.PROJECT_ID,
+      private_key_id: process.env.PRIVATE_KEY_ID,
+      private_key: process.env.PRIVATE_KEY,
+      client_email: process.env.CLIENT_EMAIL,
+      client_id: process.env.CLIENT_ID,
+      auth_uri: process.env.AUTH_URI,
+      token_uri: process.env.TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+      client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+      universe_domain: process.env.UNIVERSE_DOMAIN,
+    } as admin.ServiceAccount;
+
+    return admin.initializeApp({
+      credential: admin.credential.cert(firebaseConfig),
+      databaseURL: `https://${firebaseConfig.projectId}.firebaseio.com`,
+      storageBucket: `${firebaseConfig.projectId}.appspot.com`,
+    });
+  },
+};
+
+@Module({
+  imports: [ConfigModule],
+  providers: [firebaseProvider],
+  exports: [],
+})
+export class FirebaseModule {}
