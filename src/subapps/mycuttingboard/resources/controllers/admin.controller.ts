3daf20405546d5b4a1edcb29a582cf6e87f3ba63
JCWoodworker <jc@rilocalwoodworks.com>
2024-06-02 22:25:22 -0400

    added logic to save new boards or coasters

--- src/subapps/mycuttingboard/resources/controllers/admin.controller.ts
+++ src/subapps/mycuttingboard/resources/controllers/admin.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get, Param, Post, Body, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AdminService } from '../services/admin.service';
 import { Roles } from 'src/iam/authorization/decorators/roles.decorator';
 import { Role } from 'src/users/enums/role.enum';
-
 import { MycuttingboardBoards } from '../../entities/mycuttingboardBoards.entity';
 import { MycuttingboardCoasters } from '../../entities/mycuttingboardCoasters.entity';
+
+type NewProductType = {
+  category: string;
+  newProduct: MycuttingboardBoards | MycuttingboardCoasters;
+};
 
 @Roles(Role.Admin)
 @Controller('admin')
@@ -21,33 +25,8 @@ export class AdminController {
     return await this.adminService.getAllProductData();
   }
 
-  @Get('board/:id')
-  async getBoardDataById(@Param('id') id: number) {
-    return await this.adminService.getBoardDataById(id);
-  }
-
-  @Post('board')
-  async addBoardData(@Body() boardData: MycuttingboardBoards) {
-    return await this.adminService.addBoardData(boardData);
-  }
-
-  @Delete('board/:id')
-  async deleteBoardData(@Param('id') id: number) {
-    return await this.adminService.deleteBoardData(id);
-  }
-
-  @Get('coaster/:id')
-  async getCoasterDataById(@Param('id') id: number) {
-    return await this.adminService.getCoasterDataById(id);
-  }
-
-  @Post('coaster')
-  async addCoasterData(@Body() coasterData: MycuttingboardCoasters) {
-    return await this.adminService.addCoasterData(coasterData);
-  }
-
-  @Delete('coaster/:id')
-  async deleteCoasterData(@Param('id') id: number) {
-    return await this.adminService.deleteCoasterData(id);
+  @Post('add-new-product')
+  async addNewProduct(@Body() newProduct: NewProductType) {
+    return await this.adminService.addNewProduct(newProduct);
   }
 }
