b625a657788ac0d882e59d96697669fa04ab8542
JCWoodworker <jc@rilocalwoodworks.com>
2024-06-06 23:31:22 -0400

    updated endpoints and moved product actions over to admin

--- src/subapps/mycuttingboard/resources/controllers/product.controller.ts
+++ src/subapps/mycuttingboard/resources/controllers/product.controller.ts
@@ -1,15 +1,7 @@
-import { UpdateProductDto } from './../../dto/update-product.dto';
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ProductService } from '../services/product.service';
-import { CreateProductDto } from '../../dto/create-product.dto';
+import { Auth } from 'src/iam/decorators/auth.decorator';
+import { AuthType } from 'src/iam/enums/auth-type.enum';
 
 @Controller('product')
 export class ProductController {
@@ -20,26 +12,9 @@ export class ProductController {
     return await this.productService.getAllProducts();
   }
 
+  @Auth(AuthType.None)
   @Get(':id')
   async getProductById(@Param('id') id: number) {
     return await this.productService.getProductById(id);
-  }
-
-  @Post('new')
-  async addNewProduct(@Body() newProduct: CreateProductDto) {
-    return await this.productService.addNewProduct(newProduct);
-  }
-
-  @Delete('delete/:id')
-  async deleteProduct(@Param('id') id: number) {
-    return await this.productService.deleteProduct(id);
-  }
-
-  @Patch('update/:id')
-  async updateProduct(
-    @Param('id') id: number,
-    @Body() productUpdateData: UpdateProductDto,
-  ) {
-    return await this.productService.updateProduct(id, productUpdateData);
   }
 }
--- src/subapps/mycuttingboard/resources/services/admin.service.ts
+++ src/subapps/mycuttingboard/resources/services/admin.service.ts
@@ -1,59 +1,48 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-import { BoardsService } from './boards.service';
-import { CoastersService } from './coasters.service';
-import { MycuttingboardBoards } from '../../entities/mycuttingboardBoards.entity';
-import { MycuttingboardCoasters } from '../../entities/mycuttingboardCoasters.entity';
+import { CbcProduct } from '../../entities/cbcProducts.entity';
+import { UpdateProductDto } from '../../dto/update-product.dto';
 
 @Injectable()
 export class AdminService {
   constructor(
-    @InjectRepository(MycuttingboardBoards)
-    private boardsRepository: Repository<MycuttingboardBoards>,
-    @InjectRepository(MycuttingboardCoasters)
-    private coastersRepository: Repository<MycuttingboardCoasters>,
-    private boardsService: BoardsService,
-    private coastersService: CoastersService,
+    @InjectRepository(CbcProduct)
+    private productRepository: Repository<CbcProduct>,
   ) {}
 
-  async getTestMessage() {
-    return {
-      message: 'This message will only show to users with admin access',
-    };
-  }
-
   async getAllProductData() {
-    const boards = await this.boardsService.getAllBoardData();
-    const coasters = await this.coastersService.getAllCoasterData();
-    // const woods = await this.woodsService.getAllWoodsData();
-    return { boards, coasters };
+    return await this.productRepository.find();
   }
 
-  async addNewProduct(product: any) {
-    if (product.category === 'boards') {
-      return await this.boardsRepository.save(product.newProduct);
-    } else if (product.category === 'coasters') {
-      return await this.coastersRepository.save(product.newProduct);
-    } else {
-      throw new HttpException(
-        'Invalid product type',
-        HttpStatus.UNPROCESSABLE_ENTITY,
-      );
+  async addNewProduct(newProduct) {
+    try {
+      await this.productRepository.save(newProduct);
+      return { message: 'Product added successfully', product: newProduct };
+    } catch (error) {
+      return false;
     }
   }
 
-  async deleteProduct(id: number, category: string) {
-    if (category === 'boards') {
-      return await this.boardsRepository.delete({ id });
-    } else if (category === 'coasters') {
-      return await this.coastersRepository.delete({ id });
-    } else {
-      throw new HttpException(
-        'Invalid product type',
-        HttpStatus.UNPROCESSABLE_ENTITY,
-      );
+  async updateProduct(id: number, productUpdateData: UpdateProductDto) {
+    const product = await this.productRepository.findOneBy({ id });
+    return await this.productRepository.save({
+      ...product,
+      ...productUpdateData,
+    });
+  }
+
+  async deleteProduct(id: number) {
+    try {
+      const product = await this.productRepository.findOneBy({ id });
+      if (!product) {
+        return { message: `Product with id ${id} not found` };
+      }
+      await this.productRepository.delete({ id });
+      return { message: `Successfully deleted - ${product.title}` };
+    } catch (error) {
+      return { message: `Failed to delete - ${error}` };
     }
   }
 }
