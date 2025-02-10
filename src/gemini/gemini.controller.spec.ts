83007bd2f4fc044d3ad8e4d176ede600acc5b36e
JCWoodworker <jc@rilocalwoodworks.com>
2025-02-09 22:55:02 -0500

    cleaned up gemini resources and added controller test

--- src/gemini/gemini.controller.spec.ts
+++ src/gemini/gemini.controller.spec.ts
@@ -4,17 +4,33 @@ import { GeminiService } from './gemini.service';
 
 describe('GeminiController', () => {
   let controller: GeminiController;
+  let service: GeminiService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GeminiController],
-      providers: [GeminiService],
+      providers: [
+        {
+          provide: GeminiService,
+          useValue: {
+            getTestMessage: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<GeminiController>(GeminiController);
+    service = module.get<GeminiService>(GeminiService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return a test message', async () => {
+    const result = 'This action returns all gemini';
+    jest.spyOn(service, 'getTestMessage').mockResolvedValue(result);
+
+    expect(await controller.getTestMessage()).toBe(result);
   });
 });
--- src/gemini/gemini.controller.ts
+++ src/gemini/gemini.controller.ts
@@ -1,46 +1,12 @@
-import {
-  Controller,
-  Get,
-  // Post,
-  // Body,
-  // Patch,
-  Param,
-  // Delete,
-} from '@nestjs/common';
-// import { CreateGeminiDto } from './dto/create-gemini.dto';
-// import { UpdateGeminiDto } from './dto/update-gemini.dto';
+import { Controller, Get } from '@nestjs/common';
 
 import { GeminiService } from './gemini.service';
-
-// import { Roles } from 'src/iam/authorization/decorators/roles.decorator';
-// import { Role } from 'src/users/enums/role.enum';
-
 @Controller()
 export class GeminiController {
   constructor(private readonly geminiService: GeminiService) {}
 
-  // @Post()
-  // create(@Body() createGeminiDto: CreateGeminiDto) {
-  //   return this.geminiService.create(createGeminiDto);
-  // }
-
   @Get()
-  async findAll() {
-    return this.geminiService.findAll();
+  async getTestMessage() {
+    return this.geminiService.getTestMessage();
   }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.geminiService.findOne(+id);
-  }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateGeminiDto: UpdateGeminiDto) {
-  //   return this.geminiService.update(+id, updateGeminiDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.geminiService.remove(+id);
-  // }
 }
