1faede53c5f9e455fea018aff5427c392c3dce14
JCWoodworker <jc@rilocalwoodworks.com>
2025-04-03 11:42:31 -0400

    customized throttle for scraper GET endpoint

--- src/scrapers/scrapers.controller.ts
+++ src/scrapers/scrapers.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { Throttle } from '@nestjs/throttler';
 
 import { Auth } from '@iam/decorators/auth.decorator';
 import { AuthType } from '@iam/enums/auth-type.enum';
@@ -10,6 +11,7 @@ import { ScrapersService } from './scrapers.service';
 export class ScrapersController {
   constructor(private readonly scrapersService: ScrapersService) {}
 
+  @Throttle({ default: { limit: 30, ttl: 60000 } })
   @Get(':id')
   findScraper(@Param('id') id: string): Promise<any> {
     return this.scrapersService.findScraper(id);
