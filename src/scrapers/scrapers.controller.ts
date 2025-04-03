0d1f2422502891790cdb6bd57ecc0f8c2341a589
JCWoodworker <jc@rilocalwoodworks.com>
2025-04-03 11:52:38 -0400

    allowing localhost 5173 on scrapers GET endpoint

--- src/scrapers/scrapers.controller.ts
+++ src/scrapers/scrapers.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Header } from '@nestjs/common';
 import { Throttle } from '@nestjs/throttler';
 
 import { Auth } from '@iam/decorators/auth.decorator';
@@ -12,6 +12,10 @@ export class ScrapersController {
   constructor(private readonly scrapersService: ScrapersService) {}
 
   @Throttle({ default: { limit: 30, ttl: 60000 } })
+  @Header('Access-Control-Allow-Origin', 'http://localhost:5173')
+  @Header('Access-Control-Allow-Methods', 'GET')
+  @Header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  @Header('Access-Control-Allow-Credentials', 'true')
   @Get(':id')
   findScraper(@Param('id') id: string): Promise<any> {
     return this.scrapersService.findScraper(id);
