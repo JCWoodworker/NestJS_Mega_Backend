67a95d08b222f1a733a04b6c8992fde59cee367d
JCWoodworker <jc@rilocalwoodworks.com>
2025-04-19 12:36:36 -0400

    added test GET and GET by business ID

--- src/subapps/ironview/ironview.controller.ts
+++ src/subapps/ironview/ironview.controller.ts
@@ -1,16 +1,21 @@
 import {
   Controller,
-  // Get,
+  Get,
   // Post,
   // Body,
   // Patch,
-  // Param,
+  Param,
   // Delete,
 } from '@nestjs/common';
+import { UUID } from 'crypto';
+
+import { Auth } from '@iam/decorators/auth.decorator';
+import { AuthType } from '@iam/enums/auth-type.enum';
 
 import { IronviewService } from './ironview.service';
 
-@Controller('ironview')
+@Auth(AuthType.None)
+@Controller('')
 export class IronviewController {
   constructor(private readonly ironviewService: IronviewService) {}
 
@@ -19,15 +24,18 @@ export class IronviewController {
   //   return this.ironviewService.create(createIronview);
   // }
 
-  // @Get()
+  @Get()
+  testGet() {
+    return 'test';
+  }
   // findAll() {
   //   return this.ironviewService.findAll();
   // }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.ironviewService.findOne(+id);
-  // }
+  @Get(':id')
+  findOne(@Param('id') id: UUID) {
+    return this.ironviewService.findOne(id);
+  }
 
   // @Patch(':id')
   // update(@Param('id') id: string, @Body() updateIronview) {
--- src/subapps/ironview/ironview.service.ts
+++ src/subapps/ironview/ironview.service.ts
@@ -1,16 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UUID } from 'crypto';
+import { Repository } from 'typeorm';
+
+import { Building } from './entities/building.entity';
 
 @Injectable()
 export class IronviewService {
+  constructor(
+    @InjectRepository(Building)
+    private buildingRepository: Repository<Building>,
+  ) {}
   // create(ironview) {
   //   return `This action adds a new ironview - ${ironview}`;
   // }
   // findAll() {
   //   return `This action returns all ironview`;
   // }
-  // findOne(id: number) {
-  //   return `This action returns a #${id} ironview`;
-  // }
+  async findOne(id: UUID) {
+    const building = await this.buildingRepository.findOne({
+      where: {
+        id,
+      },
+      relations: [
+        'floors',
+        'floors.areas',
+        'floors.areas.rooms',
+        'floors.areas.rooms.walls',
+        'floors.areas.rooms.walls.images',
+      ],
+    });
+    return building;
+  }
   // update(id: number, updateIronview) {
   //   return `This action updates a #${id} ironview - ${updateIronview}`;
   // }
